5fcbf25fe915b96c2e264209cdd5de0c886e06b2
dera1337 <yoris.mourinho@yahoo.com>
2022-02-22 21:49:30 +0700

    finalize

--- app/src/main/java/com/example/tenthbottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/tenthbottomnavigationview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.tenthbottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tenthbottomnavigationview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHost) as NavHostFragment
+
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+        binding.btnNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
